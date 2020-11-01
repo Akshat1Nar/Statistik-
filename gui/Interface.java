3b0b34fa4f00b83da7dadd6b4ceac34cad388449
Akshat1Nar <akshatdixit51@gmail.com>
2020-11-01 16:53:27 +0530

    Added Window basic

---
+++ gui/Interface.java
@@ -0,0 +1,15 @@
+
+import java.awt.*;
+
+public class Interface extends Frame{
+
+	Interface(){
+		super();
+		setSize(800,600);
+		setVisible(true);
+	}
+
+	public static void main(String[] args){
+		new Interface();
+	}
+}
