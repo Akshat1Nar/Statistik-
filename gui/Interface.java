b8bb156d345b222620d9deb19ba76f46a3c3df6b
Akshat1Nar <akshatdixit51@gmail.com>
2020-11-02 19:52:21 +0530

    Added map functionality

--- gui/Interface.java
+++ gui/Interface.java
@@ -1,15 +1,44 @@
+package gui;
+import processing.core.PApplet;
+import de.fhpotsdam.unfolding.UnfoldingMap; 
+import de.fhpotsdam.unfolding.geo.Location; 
+import de.fhpotsdam.unfolding.providers.AbstractMapProvider; 
+import de.fhpotsdam.unfolding.providers.Google; 
+import de.fhpotsdam.unfolding.providers.OpenStreetMap; 
+import de.fhpotsdam.unfolding.providers.MBTilesMapProvider; 
+import de.fhpotsdam.unfolding.utils.MapUtils; 
 
-import java.awt.*;
+public class Interface extends PApplet{
+	private static final long serialVersionUID = 1L; 
+  	private static int mapWidth = 350; 
+    private static int mapHeight = 500; 
+    String tilesStr = sketchPath("./Unfolding_Maps/data/myMapStyle.mbtiles");
 
-public class Interface extends Frame{
+	UnfoldingMap map;
 
-	Interface(){
-		super();
-		setSize(800,600);
-		setVisible(true);
+	public void setup() 
+    {
+        this.size(900, 600); 
+  		this.background(0, 0, 128); 
+  		AbstractMapProvider provider = new OpenStreetMap.OpenStreetMapProvider(); 
+  
+        int zoomLevel = 7; 
+  
+        map = new UnfoldingMap(this, 40, 50, mapWidth, mapHeight, provider);
+        // map = new UnfoldingMap(this, new MBTilesMapProvider(tilesStr));
+        map.zoomAndPanTo( zoomLevel, new Location(28.7041f, 77.1025f)); 
+        MapUtils.createDefaultEventDispatcher( this, map); 
 	}
-
+  
+    // Function to draw the applet window 
+    public void draw() 
+    { 
+        map.draw();
+    } 
+	
 	public static void main(String[] args){
-		new Interface();
+		String[] processingArgs = {"Interface"};
+		Interface face = new Interface();
+		PApplet.runSketch(processingArgs, face);
 	}
 }
--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
+export CLASSPATH=$CLASSPATH:./mysql-connector-java-8.0.22/mysql-connector-java-8.0.22.jar
+export CLASSPATH=$CLASSPATH:./Unfolding_Maps/lib/*
+export CLASSPATH=$CLASSPATH:./Processing/*
+
 javac setup/Setup.java
 java setup/Setup
 
+javac gui/Interface.java
+java gui/Interface
